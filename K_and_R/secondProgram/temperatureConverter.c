a6de5655dafad97b1bf28ea52893ac70f65dcb9a
Luke <lukedmcalpine@gmail.com>
2023-07-06 16:23:06 -0700

    Fixed typo in print statement for float

--- K_and_R/secondProgram/temperatureConverter.c
+++ K_and_R/secondProgram/temperatureConverter.c
@@ -12,14 +12,16 @@ main()
     step = 20;
 
     fahr = lower;
+    printf("Fahrenheight-Celsius table\n");
     while (fahr <= upper)
     {
-        celsius = (5.0/9.0)*(fahr-32.0);
+
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
         // Each % in the first argument is paired with the corresponding second argument, third argument, etc
         // must match up properly by number and type
         // print first number of each line in a field three digits wide
         // print the second number in a field six digits wide
-        printf("%3.0d\t%6.0d\n", fahr, celsius);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
