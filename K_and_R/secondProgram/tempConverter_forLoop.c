2b50f5c01a4ccac399641d363538b260f4e05974
Luke <lukedmcalpine@gmail.com>
2023-07-06 17:27:32 -0700

    added constants for temp conversion

--- K_and_R/secondProgram/tempConverter_forLoop.c
+++ K_and_R/secondProgram/tempConverter_forLoop.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 /*print Fahrenheight-Celsius table
     for fahr = 0, 20, ..., 300*/
 main()
@@ -7,6 +12,6 @@ main()
 
     int fahr;
 
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
         printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }
